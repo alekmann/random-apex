4cc7e9b69805e93b9cbd9f74f06144c152bcf258
aleksander <aleksbro@stud.ntnu.no>
2023-06-05 21:29:15 +0200

    changes

--- src/components/LegendBox.vue
+++ src/components/LegendBox.vue
@@ -1,29 +1,84 @@
 <template>
-<div id="legend-box">
-  <img id="legend-image" :src="require('@/assets/' + imageName + '')" alt="Legend"/>
-</div>
-</template>
-
-<script>
-export default {
+    <div id="legend-box">
+      <div
+        class="shot-image"
+        ref="shotImage"
+        v-show="isShot"
+        @click="revealImage"
+        :style="{ backgroundImage: 'url(' + shotImageSrc + ')' }"
+        v-if="!revealed"
+      ></div>
+      <img
+        id="legend-image"
+        :src="require('@/assets/' + imageName + '')"
+        alt="Legend"
+        v-if="isShot ? revealed : true"
+      />
+    </div>
+  </template>
+  
+  <script>
+  export default {
     props: {
-        imageName: String,
-        legendName: String,
-    }
-}
-</script>
-
-<style>
-#legend-box {
+      imageName: String,
+      legendName: String,
+      revealed: Boolean,
+    },
+    data() {
+      return {
+        revealed: false,
+      };
+    },
+    methods: {
+      revealImage() {
+        this.revealed = !this.revealed;
+      },
+    },
+  
+    computed: {
+      isShot() {
+        return (
+          this.imageName === "Benji.jpeg" ||
+          this.imageName === "Snollo.jpeg" ||
+          this.imageName === "Sonny.jpeg" ||
+          this.imageName === "Aleks.jpeg"
+        );
+      },
+      shotImageSrc() {
+        return require('@/assets/shot.jpeg');
+      },
+    },
+  }
+  </script>
+  
+  <style>
+  #legend-box {
+    position: relative;
     width: 230px;
     height: 350px;
     border: 5px solid gray;
     margin: 2px;
     border-radius: 4px;
-}
-#legend-image {
+  }
+  
+  .shot-image {
+    position: absolute;
+    top: 0;
+    left: 0;
     width: 100%;
     height: 100%;
-}
-
-</style>
+    z-index: 2;
+ 
+    background-size: contain;
+  }
+  
+  #legend-image {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+  }
+  </style>
+  
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,20 +4,20 @@
   <div class="components-grid">
       <div class="flex-wrap">
           <div class="legend-box-div">
-              <legend-box :imageName="image1"/>
+            <legend-box :imageName="image1" :class="{ 'shot-selected': isShotSelected(image1) }" :key="imageKey1" :revealed="!isShotSelected(image1)"/>
             </div>
             <div><h2> {{ imageText1 }} </h2></div>
       </div>
       <div class="flex-wrap">
           <div class="legend-box-div">
-              <legend-box :imageName="image2"/>
-            </div>
+            <legend-box :imageName="image2" :class="{ 'shot-selected': isShotSelected(image2) }" :key="imageKey2" :revealed="!isShotSelected(image2)"/>
+        </div>
             <div><h2> {{ imageText2 }} </h2></div>
       </div>
       <div class="flex-wrap">
           <div class="legend-box-div">
-              <legend-box :imageName="image3"/>
-            </div>
+            <legend-box :imageName="image3" :class="{ 'shot-selected': isShotSelected(image3) }" :key="imageKey3" :revealed="!isShotSelected(image3)"/>
+        </div>
             <div><h2> {{ imageText3 }} </h2></div>
       </div>
   </div>
@@ -33,8 +33,13 @@
 
 <script>
 import LegendBox from '@/components/LegendBox.vue'
+import legends from '@/assets/legends.json';
 export default {
   components: { LegendBox },
+
+  mounted() {
+    this.preloadImages();
+  },
 
    data() {
         return {
@@ -45,77 +50,139 @@ export default {
             imageText2: "",
             image3: "apex-icon.jpeg",
             imageText3: "",
-            legendListArray: [
-                'Ash.jpeg',
-                'Bangalore.jpeg',
-                'Bloodhound.jpeg',
-                'Caustic.jpeg',
-                'Crypto.jpeg',
-                'Fuse.jpeg',
-                'Gibraltar.jpeg',
-                'Horizon.jpeg',
-                'Lifeline.jpeg',
-                'Loba.jpeg',
-                'Mad-maggie.jpeg',
-                'Mirage.jpeg',
-                'Newcastle.jpeg',
-                'Octane.jpeg',
-                'Pathfinder.jpeg',
-                'Rampart.jpeg',
-                'Revenant.jpeg',
-                'Seer.jpeg',
-                'Valkyrie.jpeg',
-                'Wattson.jpeg',
-                'Wraith.jpeg'
-            ]
+            legendListArray: legends,
+            imageKey1: 0,
+            imageKey2: 0,
+            imageKey3: 0,
         }
     },
 
-
   methods: {
-      randomLegend() {
-        let x = 0;
-        for(let i = 0; i < this.shots; i++) {
-            if(x == 0) {
-                this.legendListArray.push("Benji.jpeg");
-                x++;
-              } else if(x == 1) {
-                  this.legendListArray.push("Snollo.jpeg");
-                  x++;
-              } else if(x == 2) {
-                  this.legendListArray.push("Sonny.jpeg");
-                  x++;
-              } else if(x == 3) {
-                  this.legendListArray.push("Aleks.jpeg");
-                  x = 0;
-              }
+     shuffle(array) {
+        let currentIndex = array.length,
+            temporaryValue,
+            randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
         }
-        this.image1 = this.getRandomItem(this.legendList);
-        if(this.image1 == "Benji.jpeg" || this.image1 == "Snollo.jpeg" || this.image1 == "Sonny.jpeg" || this.image1 == "Aleks.jpeg") {
-            this.imageText1 = "Shoooooot!!!"
-        } else {
-            this.imageText1 = this.image1.replace('.jpeg', "");
+
+        return array;
+        },
+    
+    randomLegend() {
+         // Reset legendListArray to its initial state
+        this.legendListArray = [...legends];
+
+        const friends = ["Benji.jpeg", "Snollo.jpeg", "Sonny.jpeg", "Aleks.jpeg"];
+
+        // Create an extendedList containing legends and a fair distribution of shots
+        const extendedList = [...this.legendListArray];
+        const numLegends = this.legendListArray.length;
+        const numImages = numLegends + this.shots;
+
+        let shotsToInsert = this.shots;
+        while (shotsToInsert > 0) {
+        const randomFriend = friends[Math.floor(Math.random() * friends.length)];
+        const randomIndex = Math.floor(Math.random() * (numImages - shotsToInsert));
+        extendedList.splice(randomIndex, 0, randomFriend);
+        shotsToInsert--;
         }
-        
-        this.image2 = this.getRandomItem(this.legendList);
-        if(this.image2 == "Benji.jpeg" || this.image2 == "Snollo.jpeg" || this.image2 == "Sonny.jpeg" || this.image2 == "Aleks.jpeg") {
-            this.imageText2 = "Shoooooot!!!"
-        } else {
-            this.imageText2 = this.image2.replace('.jpeg', "");
+
+const getRandomItem = (list) => {
+  return list[Math.floor(Math.random() * list.length)];
+};
+
+        const getImageAndText = () => {
+            const image = getRandomItem(extendedList);
+            const isShot = friends.includes(image);
+            const imageText = isShot ? "Shoooooot!!!" : image.replace('.jpeg', "");
+            return { image, imageText };
+        };
+
+        const result1 = getImageAndText();
+        const result2 = getImageAndText();
+        const result3 = getImageAndText();
+
+        this.image1 = result1.image;
+        this.imageText1 = result1.imageText;
+        this.image2 = result2.image;
+        this.imageText2 = result2.imageText;
+        this.image3 = result3.image;
+        this.imageText3 = result3.imageText;
+        this.imageKey1 = Math.random();
+        this.imageKey2 = Math.random();
+        this.imageKey3 = Math.random();
+        },
+
+
+    getRandomItem() {
+        return this.legendListArray[Math.floor(Math.random()*this.legendListArray.length)];
+    },
+
+    isShotSelected(imageName) {
+        const shotImages = ["Benji.jpeg", "Snollo.jpeg", "Sonny.jpeg", "Aleks.jpeg"];
+        return shotImages.includes(imageName);
+  },
+
+    simulateRandomSelection(numSimulations, numShots) {
+        const friends = ["Benji.jpeg", "Snollo.jpeg", "Sonny.jpeg", "Aleks.jpeg"];
+        const count = { "Benji.jpeg": 0, "Snollo.jpeg": 0, "Sonny.jpeg": 0, "Aleks.jpeg": 0 };
+
+        for (let i = 0; i < numSimulations; i++) {
+            // Reset image variables
+            this.shots = numShots;
+            this.image1 = "";
+            this.image2 = "";
+            this.image3 = "";
+            this.image4 = "";
+
+            // Call randomLegend method
+            this.randomLegend();
+
+            // Count the friends' images
+            if (friends.includes(this.image1)) {
+            count[this.image1]++;
+            }
+            if (friends.includes(this.image2)) {
+            count[this.image2]++;
+            }
+            if (friends.includes(this.image3)) {
+            count[this.image3]++;
+            }
+            if (friends.includes(this.image4)) {
+            count[this.image4]++;
+            }
         }
-        
-        this.image3 = this.getRandomItem(this.legendList);
-        if(this.image3 == "Benji.jpeg" || this.image3 == "Snollo.jpeg" || this.image3 == "Sonny.jpeg" || this.image3 == "Aleks.jpeg") {
-            this.imageText3 = "Shoooooot!!!"
-        } else {
-            this.imageText3 = this.image3.replace('.jpeg', "");
-        }
-        this.legendListArray.splice(21, this.legendListArray.length - 21);
-      },
-        getRandomItem() {
-            return this.legendListArray[Math.floor(Math.random()*this.legendListArray.length)];
-        }
+
+        console.log("Results after", numSimulations, "simulations with", numShots, "shots:");
+        console.log(count);
+        },
+
+    preloadImages() {
+        const friends = ["Benji.jpeg", "Snollo.jpeg", "Sonny.jpeg", "Aleks.jpeg"];
+        const imagesToPreload = [...this.legendListArray, ...friends];
+
+        imagesToPreload.forEach((imageName) => {
+            const image = new Image();
+            image.src = require('@/assets/' + imageName);
+        });
+    },
+
+
 },
+//created() {
+//  window.simulateRandomSelection = this.simulateRandomSelection;
+//}
+
 }
 </script>
 
@@ -133,6 +200,7 @@ body {
 .components-grid {
     display: flex;
     justify-content: center;
+    flex-wrap: wrap;
 }
 .button-shots-flex {
     display: flex;
@@ -182,5 +250,41 @@ h1 {
     color:darkslategrey;
     font-size: 45px;
 }
+
+@keyframes dropIn {
+  0% {
+    opacity: 0;
+    transform: translateY(-100px) rotate(45deg);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0) rotate(0);
+  }
+}
+
+@keyframes shake {
+  0%,
+  100% {
+    transform: translateX(0);
+  }
+  10%,
+  30%,
+  50%,
+  70%,
+  90% {
+    transform: translateX(-3px);
+  }
+  20%,
+  40%,
+  60%,
+  80% {
+    transform: translateX(3px);
+  }
+}
+
+.shot-selected {
+  animation: dropIn 0.5s ease-out, shake 0.5s ease-in-out 0.5s;
+}
+
 
 </style>
